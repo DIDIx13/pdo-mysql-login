a1be292ec190a21eb4625955f321af26d575c755
Darwin Ameli <DIDI.ameli@gmail.com>
2018-03-16 17:05:20 +0100

    Update login.php

--- login.php
+++ login.php
@@ -15,7 +15,7 @@
 
 		if($errMsg == '') {
 			try {
-				$stmt = $connect->prepare('SELECT id, fullname, username, password, secretpin FROM pdo WHERE username = :username');
+				$stmt = $connect->prepare('SELECT id, fullname, username, password, secretpin FROM test WHERE username = :username');
 				$stmt->execute(array(
 					':username' => $username
 					));
@@ -56,13 +56,13 @@
 <body>
 	<div align="center">
 		<div style=" border: solid 1px #006D9C; " align="left">
-			<?php
-				if(isset($errMsg)){
-					echo '<div style="color:#FF0000;text-align:center;font-size:17px;">'.$errMsg.'</div>';
-				}
-			?>
 			<div style="background-color:#006D9C; color:#FFFFFF; padding:10px;"><b>Login</b></div>
 			<div style="margin: 15px">
+				<?php
+					if(isset($errMsg)){
+						echo '<div style="color:#FF0000;text-align:center;font-size:17px;">'.$errMsg.'</div>';
+					}
+				?>
 				<form action="" method="post">
 					<input type="text" name="username" value="<?php if(isset($_POST['username'])) echo $_POST['username'] ?>" autocomplete="off" class="box"/><br /><br />
 					<input type="password" name="password" value="<?php if(isset($_POST['password'])) echo $_POST['password'] ?>" autocomplete="off" class="box" /><br/><br />
